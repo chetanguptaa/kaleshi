f66a864c332e2427f5b143e6f697c3de0d28afb2
Chetan Gupta <chetanguptaaaa21@gmail.com>
2026-01-15 19:26:28 +0530

    removed target

--- apps/matching-engine/target/debug/deps/matching_engine-81675d576bda0409.d
+++
@@ -1,5 +0,0 @@
-/Users/chetangupta/coding/projects/kalshi/apps/matching-engine/target/debug/deps/matching_engine-81675d576bda0409.d: src/main.rs
-
-/Users/chetangupta/coding/projects/kalshi/apps/matching-engine/target/debug/deps/libmatching_engine-81675d576bda0409.rmeta: src/main.rs
-
-src/main.rs:
