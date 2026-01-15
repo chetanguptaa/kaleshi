e65013d68ebbe488dbb01856945fbb5c2fb23d4e
Chetan Gupta <chetanguptaaaa21@gmail.com>
2026-01-15 18:59:54 +0530

    new changs

--- apps/backend/src/redis/redis.event-types.ts
+++ apps/backend/src/redis/redis.event-types.ts
@@ -1,9 +1,11 @@
+import { OrderSide } from 'generated/prisma/enums';
+
 export type OrderNewEvent = {
   type: 'order.new';
   orderId: string;
   marketId: number;
   outcomeId: string;
-  side: 'BUY' | 'SELL';
+  side: OrderSide;
   price: number;
   quantity: number;
   accountId: string;
