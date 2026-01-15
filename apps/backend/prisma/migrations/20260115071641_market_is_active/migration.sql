77d9d0c903b59822d80b2b6a2eb6183e3e1ffdf7
Chetan Gupta <chetanguptaaaa21@gmail.com>
2026-01-15 13:31:48 +0530

    appropriate name

---
+++ apps/backend/prisma/migrations/20260115071641_market_is_active/migration.sql
@@ -0,0 +1,23 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Market" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "startsAt" DATETIME NOT NULL,
+    "endsAt" DATETIME NOT NULL,
+    "marketCategoryId" INTEGER NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "information" JSONB,
+    "ruleBook" TEXT,
+    "rules" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Market_marketCategoryId_fkey" FOREIGN KEY ("marketCategoryId") REFERENCES "MarketCategory" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Market" ("createdAt", "endsAt", "id", "information", "marketCategoryId", "name", "ruleBook", "rules", "startsAt", "updatedAt") SELECT "createdAt", "endsAt", "id", "information", "marketCategoryId", "name", "ruleBook", "rules", "startsAt", "updatedAt" FROM "Market";
+DROP TABLE "Market";
+ALTER TABLE "new_Market" RENAME TO "Market";
+CREATE UNIQUE INDEX "Market_name_startsAt_endsAt_key" ON "Market"("name", "startsAt", "endsAt");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
